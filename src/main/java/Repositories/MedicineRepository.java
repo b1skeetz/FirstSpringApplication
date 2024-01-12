73ad441da3ce788a87cf747bd101d47501292a65
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-01-13 00:08:05 +0600

    preporation

--- src/main/java/Repositories/MedicineRepository.java
+++ src/main/java/Repositories/MedicineRepository.java
@@ -1,10 +1,7 @@
 package Repositories;
 
-import model.Medicine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface MedicineRepository extends JpaRepository<Medicine, Long> {
--- src/main/java/damir/kad/medicine/MedicineApplication.java
+++ src/main/java/damir/kad/medicine/MedicineApplication.java
@@ -1,40 +1,11 @@
 package damir.kad.medicine;
 
-
-import Services.CategoryService;
-import model.Category;
-import model.Medicine;
-import Services.MedicineService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
-@RestController
-@EntityScan(basePackages = {"model"})
 public class MedicineApplication {
-
-    private CategoryService categoryService;
-    private MedicineService medicineService;
-
     public static void main(String[] args) {
         SpringApplication.run(MedicineApplication.class, args);
-    }
-
-    @GetMapping("/index")
-    public String index(Model model) {
-        List<Category> categories = categoryService.findAll();
-        List<Medicine> medicines = medicineService.findAll();
-
-        model.addAttribute("categories", categories);
-        model.addAttribute("medicines", medicines);
-
-        return "index";
     }
 }
