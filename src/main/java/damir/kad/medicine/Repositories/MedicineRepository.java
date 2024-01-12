debc940a18884440da88111cb70aeaf21c90fe9b
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-01-13 00:24:49 +0600

    repositories added

--- src/main/java/damir/kad/medicine/Repositories/MedicineRepository.java
+++ src/main/java/damir/kad/medicine/Repositories/MedicineRepository.java
@@ -1,8 +1,12 @@
 package damir.kad.medicine.Repositories;
 
+import damir.kad.medicine.model.Medicine;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MedicineRepository extends JpaRepository<Medicine, Long> {
+    List<Medicine> findAllByDisplayTrue();
 }
