c631134478879bed9e2b59677a7284cbf6cd4d9c
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-01-13 00:45:27 +0600

    database script fully completed

--- src/main/sql/CreateTables.sql
+++ src/main/sql/CreateTables.sql
@@ -1,23 +1,39 @@
-create table Medicines(
-                          id serial8 primary key,
-                          name varchar(50) not null,
-                          date_buy date not null,
-                          date_expire date not null,
-                          category_id int8,
-                          description varchar(80) not null,
-                          foreign key (category_id) references Categories (id)
+create table users
+(
+    id         serial8 primary key,
+    role       int2    not null,
+    username   varchar not null,
+    password   varchar not null,
+    first_name varchar not null,
+    last_name  varchar not null
 );
 
-create table Categories(
-                           id serial8 primary key,
-                           name varchar(20) not null
+insert into users (role, username, password, first_name, last_name)
+values (0, 'admin', 'admin', 'Admin', 'Admin');
+
+drop table medicines;
+create table medicines
+(
+    id          serial8 primary key,
+    category_id int8        not null,
+    display     bool        not null,
+    name        varchar(50) not null,
+    date_buy    date        not null,
+    date_expire date        not null,
+    description varchar(80) not null,
+    price       int4    not null,
+    foreign key (category_id) references categories (id)
 );
 
-insert into Categories (name) values ('Таблетки'),
-                                     ('Капсулы'),
-                                     ('Сироп'),
-                                     ('Мазь'),
-                                     ('Наружнее применение');
+create table categories
+(
+    id   serial8 primary key,
+    name varchar(20) not null
+);
 
-insert into Medicines (name, date_buy, date_expire, category_id, description) values ('Тонзилотрен', '2023-12-05', '2027-12-01', 1, 'Гомеопатические для горла'),
-                                                                                   ('Нафтизин', '2023-10-21', '2025-04-01', 5, 'Капли в нос для снятия отёка');
+insert into categories (name)
+values ('Таблетки'),
+       ('Капсулы'),
+       ('Сироп'),
+       ('Капли'),
+       ('Спрей');
